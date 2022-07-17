c83d57c7620eb8aa96d731fc8cc53345f27165a2
Ptyshke <104887398+Ptyshke@users.noreply.github.com>
2022-07-18 01:11:15 +0500

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,3 @@
+<div class="footer">
+            <h2>Я изучил основы PHP <sub>(ну почти)</sub></h2>
+        </div>
---
+++ logo.inc.php
@@ -0,0 +1,5 @@
+    
+                <div class="logo"> 
+                <a href="https://www.php.net/"><img src="img/l.png" alt="php"></a>
+                </div>                                                 
+        
