ff3ab4149bffbc13bcb8a39dba0307b85c6658d0
Ptyshke <104887398+Ptyshke@users.noreply.github.com>
2022-07-19 14:41:34 +0500

    Delete logo.inc.php

--- logo.inc.php
+++
@@ -1,5 +0,0 @@
-    
-                <div class="logo"> 
-                <a href="https://www.php.net/"><img src="img/l.png" alt="php"></a>
-                </div>                                                 
-        
