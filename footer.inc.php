04cff55bf4f5d4255538a13dd7a7a03a6d763612
Ptyshke <104887398+Ptyshke@users.noreply.github.com>
2022-07-19 14:42:58 +0500

    Add files via upload

---
+++ footer.inc.php
@@ -0,0 +1,5 @@
+<div class="footer">
+            <h2>Я изучил основы PHP <sub>(ну почти)</sub></h2>
+            <p><a href="mailto:ptyshke@inbox.ru">Связаться со мной по E-Mail <img src="img/email.png" height="40px" width="40px"></a> <a href="https://wa.me/79226981928">Связаться со мной в WhatsApp</a> <img src="img/whatsapp.png" width="40px" height="40px"></p>
+            <p>Ptyshke ©, 2022</p>
+        </div>
---
+++ menu.inc.php
@@ -0,0 +1,7 @@
+            <nav>
+                <ul>
+                    <li><a href="#" title="В процессе наполнения"> Моё портфолио</title> </a></li>
+                    <li><a href="https://skillfactory.ru" target="_blank"> Ссылки </a></li>
+                    <li><a href="contacts.html"> Контакты </a></li>
+                </ul>    
+            </nav>
