a587126316d057334f988c701485f6d19294e7a6
Ptyshke <104887398+Ptyshke@users.noreply.github.com>
2022-07-19 14:41:50 +0500

    Delete menu.inc.php

--- menu.inc.php
+++
@@ -1,7 +0,0 @@
-            <nav>
-                <ul>
-                    <li><a href="#" title="В процессе наполнения"> Моё портфолио</title> </a></li>
-                    <li><a href="https://skillfactory.ru" target="_blank"> Ссылки </a></li>
-                    <li><a href="contacts.html"> Контакты </a></li>
-                </ul>    
-            </nav>
